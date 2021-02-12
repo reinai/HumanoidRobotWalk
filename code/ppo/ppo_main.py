3956ff8048cfa73967e2df7bd5d74510a391a191
Nikola <nikolazubic97@yahoo.com>
2021-02-12 02:00:27 +0100

    Added PPO_main and models after only 3 hours of training

---
+++ code/ppo/ppo_main.py
@@ -0,0 +1,28 @@
+# Execution of trained PPO model on "HumanoidPyBulletEnv-v0"
+import gym
+import pybulletgym
+import torch
+import time
+from ppo.actor_critic import ActorCritic
+import sys
+
+env = gym.make('HumanoidPyBulletEnv-v0')
+env.render()
+observation = env.reset()
+
+observation_dimensions = env.observation_space.shape[0]
+action_dimensions = env.action_space.shape[0]
+
+actor_neural_network = ActorCritic(input_dimensions=observation_dimensions, output_dimensions=action_dimensions)
+
+actor_neural_network.load_state_dict(torch.load("../trained_models/ppo/ppo_actor.pth"))
+
+while True:
+    action = actor_neural_network.forward(observation).detach().numpy()
+    observation, reward, is_done, info = env.step(action)
+    time.sleep(0.05)
+
+    if is_done is True:
+        env.reset()
+
+    env.render()
