d63f9d1df2d0a5264248ded05b089cd491a7c428
Nikola <nikolazubic97@yahoo.com>
2021-02-17 17:32:17 +0100

    Finished with PPO implementation. 50% of project done.

--- code/ppo/ppo_main.py
+++ code/ppo/ppo_main.py
@@ -6,6 +6,10 @@ import time
 from ppo.actor_critic import ActorCritic
 import sys
 
+# /home/nikola/Desktop/agt_projekat/HumanoidRobotWalk/code/trained_models/ppo/48 hours/ppo_actor.pth
+
+actor_model_path = input("Enter actor model file path: ")
+
 env = gym.make('HumanoidPyBulletEnv-v0')
 env.render()
 observation = env.reset()
@@ -15,7 +19,7 @@ action_dimensions = env.action_space.shape[0]
 
 actor_neural_network = ActorCritic(input_dimensions=observation_dimensions, output_dimensions=action_dimensions)
 
-actor_neural_network.load_state_dict(torch.load("../trained_models/ppo/48 hours/ppo_actor.pth"))
+actor_neural_network.load_state_dict(torch.load(actor_model_path))
 
 while True:
     action = actor_neural_network.forward(observation).detach().numpy()
