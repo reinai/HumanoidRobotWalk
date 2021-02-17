1e686bdad470ac6b5d8c4c8cf8276aef4d5d19f5
Nikola <nikolazubic97@yahoo.com>
2021-02-17 15:38:20 +0100

    Added trained models and log files. Robot learned to successfully walk.

--- code/ppo/ppo_main.py
+++ code/ppo/ppo_main.py
@@ -15,12 +15,12 @@ action_dimensions = env.action_space.shape[0]
 
 actor_neural_network = ActorCritic(input_dimensions=observation_dimensions, output_dimensions=action_dimensions)
 
-actor_neural_network.load_state_dict(torch.load("../trained_models/ppo/ppo_actor.pth"))
+actor_neural_network.load_state_dict(torch.load("../trained_models/ppo/48 hours/ppo_actor.pth"))
 
 while True:
     action = actor_neural_network.forward(observation).detach().numpy()
     observation, reward, is_done, info = env.step(action)
-    time.sleep(0.05)
+    time.sleep(0.03)
 
     if is_done is True:
         env.reset()
